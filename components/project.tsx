029bda5b437d96bd5a848d8b029fd1b67af98a86
김 형석 <neoself1105@gmail.com>
2024-01-26 17:40:59 +0900

    bugFix

--- components/project.tsx
+++ components/project.tsx
@@ -49,8 +49,9 @@ export default function Project({
                 <LuChevronRight />
               </a>
             )}
-            {links?.map((item: any) => (
+            {links?.map((item: any, id: number) => (
               <Link
+                key={id}
                 href={item.link}
                 target="_blank"
                 className="text-sm group bg-white px-4 py-3 flex  items-center rounded-lg outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
